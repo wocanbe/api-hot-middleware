0860ea204f2243c26a5a9efbf7d56d71e781813a
ying liu <584069777@qq.com>
2018-10-09 10:18:57 +0800

    fixed:  修复会对本域名的访问进行拦截的bug

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
 const creatApi = require('./lib/router')
 
 module.exports = function (apiConfig, allowOrigin, supportCROSCookie) {
-  const apiRouters = creatApi(apiConfig, allowOrigin, supportCROSCookie)
+  const apiRouters = new creatApi(apiConfig, allowOrigin, supportCROSCookie)
   return apiRouters.run()
 }
